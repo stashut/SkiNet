7f38df6bebf35c7f48e1f0b96bcd11800de71509
Stanislav Hut <stanislav.hut@solarwinds.com>
2022-09-13 21:30:11 +0300

    Fix swagger & improving swagger documentation

--- API/Startup.cs
+++ API/Startup.cs
@@ -43,10 +43,10 @@ namespace API
                     return new BadRequestObjectResult(errorResponse);
                 };
             });
-            // services.AddSwaggerGen(c =>
-            // {
-            //     c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-            // });
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -54,11 +54,12 @@ namespace API
         {
             app.UseMiddleware<ExceptionMiddleware>();
              
-            // if (env.IsDevelopment())
-            // {
-            //     app.UseSwagger();
-            //     app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
-            // }
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
+            }
+            
 
             //Not found endpoint error handler
             app.UseStatusCodePagesWithReExecute("/errors/{0}");
