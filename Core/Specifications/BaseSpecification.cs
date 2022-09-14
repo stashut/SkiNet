af14e4b238880926531cb62cea4264c7337f0d1f
Stanislav Hut <stanislav.hut@solarwinds.com>
2022-09-14 21:56:53 +0300

    Adding pagination

--- Core/Specifications/BaseSpecification.cs
+++ Core/Specifications/BaseSpecification.cs
@@ -17,12 +17,20 @@ namespace Core.Specifications
             Criteria = criteria;
         }
         public Expression<Func<T, bool>> Criteria { get; }
+        
         public List<Expression<Func<T, object>>> Includes { get; } = 
             new List<Expression<Func<T, object>>>();
 
         public Expression<Func<T, object>> OrderBy { get; private set; }
+        
         public Expression<Func<T, object>> OrderByDescending { get; private set; }
-
+        
+        public int Take { get; private set; }
+        
+        public int Skip { get; private set;}
+        
+        public bool isPagingEnabled { get; private set;}
+        
         protected void AddInclude(Expression<Func<T, object>> includeExpression)
         {
             Includes.Add(includeExpression);
@@ -36,6 +44,13 @@ namespace Core.Specifications
         protected void AddOrderByDescending(Expression<Func<T, object>> orderByDescendingExpression)
         {
             OrderByDescending = orderByDescendingExpression;
+        }
+
+        protected void ApplyPaging(int skip, int take)
+        {
+            Skip = skip;
+            Take = take;
+            isPagingEnabled = true;
         }
     }
 }
