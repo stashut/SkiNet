651fa420d094bf9e08fd3c1f88bb640df0ac76c0
Stanislav Hut <stanislav.hut@solarwinds.com>
2022-09-14 22:09:48 +0300

    Added search functionality & CORS support for API

--- Core/Specifications/ProductSpecParams.cs
+++ Core/Specifications/ProductSpecParams.cs
@@ -16,4 +16,11 @@ public class ProductSpecParams
     public int? BrandId { get; set; }
     public int? TypeId { get; set; }
     public string Sort { get; set; }
+    private string _search;
+
+    public string Search
+    {
+        get => _search;
+        set => _search = value.ToLower();
+    }
 }
--- Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
+++ Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
@@ -4,11 +4,13 @@ namespace Core.Specifications
 {
     public class ProductsWithTypesAndBrandsSpecification : BaseSpecification<Product>
     {
-        public ProductsWithTypesAndBrandsSpecification(ProductSpecParams productParams) 
-            : base(x => 
-            (!productParams.BrandId.HasValue || x.ProductBrandId == productParams.BrandId) && 
-            (!productParams.TypeId.HasValue || x.ProductTypeId == productParams.TypeId)
-                )
+        public ProductsWithTypesAndBrandsSpecification(ProductSpecParams productParams)
+            : base(x =>
+                (string.IsNullOrEmpty(productParams.Search) || x.Name.ToLower().Contains
+                    (productParams.Search)) &&
+                (!productParams.BrandId.HasValue || x.ProductBrandId == productParams.BrandId) &&
+                (!productParams.TypeId.HasValue || x.ProductTypeId == productParams.TypeId)
+            )
         {
             AddInclude(x => x.ProductType);
             AddInclude(x => x.ProductBrand);
