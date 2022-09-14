11275bd747d9bc4d5f1b7339f25d49957353d5bb
Stanislav Hut <stanislav.hut@solarwinds.com>
2022-09-14 21:03:35 +0300

    Added sorting functionality & fixed type error in Sqlite

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -24,9 +24,9 @@ namespace API.Controllers
             _mapper = mapper;
         }
         [HttpGet]
-        public async Task<ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProducts()
+        public async Task<ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProducts(string sort)
         {
-            var spec = new ProductsWithTypesAndBrandsSpecification();
+            var spec = new ProductsWithTypesAndBrandsSpecification(sort);
 
             var products = await _productsRepo.ListAsync(spec);
 
