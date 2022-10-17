89e970a5a70dc44460a5d0699bedba95a52426d1
Stanislav Hut <stanislav.hut@solarwinds.com>
2022-10-17 20:14:54 +0300

    adding IdentityDbContext

--- API/Startup.cs
+++ API/Startup.cs
@@ -2,6 +2,7 @@ using API.Extensions;
 using API.Helpers;
 using API.Middleware;
 using Infrastructure.Data;
+using Infrastructure.Identity;
 using Microsoft.EntityFrameworkCore;
 using StackExchange.Redis;
 
@@ -22,6 +23,10 @@ namespace API
             services.AddControllers();
             services.AddDbContext<StoreContext>(x =>
                 x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<AppIdentityDbContext>(x =>
+            {
+                x.UseSqlite(_config.GetConnectionString("IdentityConnection"));
+            })
 
             services.AddSingleton<IConnectionMultiplexer>(c =>
             {
