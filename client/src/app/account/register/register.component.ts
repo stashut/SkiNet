202063647786e7f017ea845143cc2b77d0070661
Stanislav Hut <stanislav.hut@solarwinds.com>
2022-11-05 10:43:06 +0200

    email async validator

--- client/src/app/account/register/register.component.ts
+++ client/src/app/account/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AsyncValidatorFn, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { of, timer } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
 import { AccountService } from '../account.service';
 
 @Component({
@@ -21,7 +23,9 @@ export class RegisterComponent implements OnInit {
   createRegisterForm () {
     this.registerForm = this.fb.group({
       displayName: [null, [Validators.required]],
-      email: [null, [Validators.required, Validators.pattern('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$')]],
+      email: [null, [Validators.required, Validators.pattern('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$')],
+      [this.validateEmailNotTaken()] 
+    ],
       password: [null, [Validators.required]]
     });
   }
@@ -33,6 +37,24 @@ export class RegisterComponent implements OnInit {
       console.log(error);
       this.errors = error.errors;
     });
+  }
+
+  validateEmailNotTaken (): AsyncValidatorFn {
+    return control => {
+      return timer(500).pipe(
+        switchMap(() => {
+          if(!control.value) {
+            return of(null);
+          }
+
+          return this.accountService.checkEmailExists(control.value).pipe(
+            map(res => {
+              return res ? {emailExists: true} : null; 
+            })
+          );
+        })
+      );
+    }
   }
 
 }
