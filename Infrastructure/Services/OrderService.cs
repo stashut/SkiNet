13e9d904f2953b6b650aaebe43cb37c2a900e4ad
Stanislav Hut <stanislav.hut@solarwinds.com>
2022-11-05 16:47:34 +0200

    implementing order get methods

--- Infrastructure/Services/OrderService.cs
+++ Infrastructure/Services/OrderService.cs
@@ -1,6 +1,7 @@
 using Core.Entities;
 using Core.Interfaces;
 using Core.OrderAggregate;
+using Core.Specifications;
 
 namespace Infrastructure.Services;
 
@@ -44,18 +45,22 @@ public class OrderService : IOrderService
         return order;
     }
 
-    public Task<IReadOnlyList<Order>> GetOrdersForUserAsync(string buyerEmail)
+    public async Task<IReadOnlyList<Order>> GetOrdersForUserAsync(string buyerEmail)
     {
-        throw new NotImplementedException();
+        var spec = new OrdersWithItemsAndOrderingSpecification(buyerEmail);
+
+        return await _unitOfWork.Repository<Order>().ListAsync(spec);
     }
 
-    public Task<Order> GetOrderByIdAsync(int id, string buyerEmail)
+    public async Task<Order> GetOrderByIdAsync(int id, string buyerEmail)
     {
-        throw new NotImplementedException();
+        var spec = new OrdersWithItemsAndOrderingSpecification(id, buyerEmail);
+
+        return await _unitOfWork.Repository<Order>().GetEntityWithSpec(spec);
     }
 
-    public Task<IReadOnlyList<DeliveryMethod>> GetDeliveryMethodsAsync()
+    public async Task<IReadOnlyList<DeliveryMethod>> GetDeliveryMethodsAsync()
     {
-        throw new NotImplementedException();
+        return await _unitOfWork.Repository<DeliveryMethod>().ListAllAsync();
     }
 }
