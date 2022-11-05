8d9b7e9a2abda1e2e5da1a9306a9138c0e4ad802
Stanislav Hut <stanislav.hut@solarwinds.com>
2022-11-05 13:22:50 +0200

    implementing create order method

--- Infrastructure/Services/OrderService.cs
+++ Infrastructure/Services/OrderService.cs
@@ -1,3 +1,4 @@
+using Core.Entities;
 using Core.Interfaces;
 using Core.OrderAggregate;
 
@@ -5,9 +6,40 @@ namespace Infrastructure.Services;
 
 public class OrderService : IOrderService
 {
-    public Task<Order> CreateOrderAsync(string buyerEmail, int deliveryMethod, string basketId, Address shippingAddress)
+    private readonly IGenericRepository<Order> _orderRepo;
+    private readonly IGenericRepository<DeliveryMethod> _dmRepo;
+    private readonly IGenericRepository<Product> _productRepo;
+    private readonly IBasketRepository _basketRepo;
+
+    public OrderService(IGenericRepository<Order> orderRepo, IGenericRepository<DeliveryMethod> dmRepo, IGenericRepository<Product> productRepo, IBasketRepository basketRepo)
     {
-        throw new NotImplementedException();
+        _orderRepo = orderRepo;
+        _dmRepo = dmRepo;
+        _productRepo = productRepo;
+        _basketRepo = basketRepo;
+    }
+
+    public async Task<Order> CreateOrderAsync(string buyerEmail, int deliveryMethodId, string basketId, Address shippingAddress)
+    {
+        var basket = await _basketRepo.GetBasketAsync(basketId);
+
+        var items = new List<OrderItem>();
+        foreach (var item in items)
+        {
+            var productItem = await _productRepo.GetByIdAsync(item.Id);
+            var itemOrdered = new ProductItemOrdered(productItem.Id, productItem.Name, productItem.PictureUrl);
+            var orderItem = new OrderItem(itemOrdered, productItem.Price, item.Quantity);
+            items.Add(orderItem);
+        }
+
+        var deliveryMethod = await _dmRepo.GetByIdAsync(deliveryMethodId);
+
+        var subtotal = items.Sum(item => item.Price * item.Quantity);
+
+        var order = new Order(items, buyerEmail, shippingAddress, deliveryMethod, subtotal);
+        //TODO: save it to db
+
+        return order;
     }
 
     public Task<IReadOnlyList<Order>> GetOrdersForUserAsync(string buyerEmail)
