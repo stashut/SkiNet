d4d401acf26d630968571bc1d4ddc4826bf28c1e
Stanislav Hut <stanislav.hut@solarwinds.com>
2022-11-06 16:27:41 +0200

    order submission

--- client/src/app/basket/basket.service.ts
+++ client/src/app/basket/basket.service.ts
@@ -87,6 +87,13 @@ export class BasketService {
     }
   }
 
+  deleteLocalBasket(id: string) {
+    this.basketSource.next(null);
+    this.basketTotalSource.next(null);
+    localStorage.removeItem('basket_id');
+    
+  }
+
   deleteBasket(basket: IBasket) {
     return this.http.delete(this.baseUrl + 'basket?id=' + basket.id).subscribe(() => {
       this.basketSource.next(null);
