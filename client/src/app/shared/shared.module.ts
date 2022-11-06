4e322d3fdbef2bd347821775e114a21fa9a5b9f1
Stanislav Hut <stanislav.hut@solarwinds.com>
2022-11-06 12:44:07 +0200

    making the basket summary shared

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -9,7 +9,9 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { TextInputComponent } from './components/text-input/text-input.component';
 import { CdkStepperModule } from '@angular/cdk/stepper';
-import { StepperComponent } from './components/stepper/stepper.component'
+import { StepperComponent } from './components/stepper/stepper.component';
+import { BasketSummaryComponent } from './components/basket-summary/basket-summary.component'
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
@@ -18,7 +20,8 @@ import { StepperComponent } from './components/stepper/stepper.component'
     PagerComponent,
     OrderTotalsComponent,
     TextInputComponent,
-    StepperComponent
+    StepperComponent,
+    BasketSummaryComponent
   ],
   imports: [
     CommonModule,
@@ -26,7 +29,8 @@ import { StepperComponent } from './components/stepper/stepper.component'
     CarouselModule.forRoot(),
     BsDropdownModule.forRoot(),
     ReactiveFormsModule,
-    CdkStepperModule
+    CdkStepperModule,
+    RouterModule
   ],
   exports: [
     PaginationModule,
@@ -38,7 +42,8 @@ import { StepperComponent } from './components/stepper/stepper.component'
     BsDropdownModule,
     TextInputComponent,
     CdkStepperModule,
-    StepperComponent
+    StepperComponent,
+    BasketSummaryComponent
   ]
 })
 export class SharedModule { }
