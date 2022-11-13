b57ba91d3b45dfb3ca4c729fc1bd8669522495db
Stanislav Hut <stanislav.hut@solarwinds.com>
2022-11-13 23:13:17 +0200

    caching on client side

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -5,7 +5,7 @@ import { PagingHeaderComponent } from './components/paging-header/paging-header.
 import { PagerComponent } from './components/pager/pager.component';
 import {CarouselModule} from 'ngx-bootstrap/carousel';
 import { OrderTotalsComponent } from './components/order-totals/order-totals.component'
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { TextInputComponent } from './components/text-input/text-input.component';
 import { CdkStepperModule } from '@angular/cdk/stepper';
@@ -30,7 +30,8 @@ import { RouterModule } from '@angular/router';
     BsDropdownModule.forRoot(),
     ReactiveFormsModule,
     CdkStepperModule,
-    RouterModule
+    RouterModule,
+    FormsModule
   ],
   exports: [
     PaginationModule,
@@ -43,7 +44,8 @@ import { RouterModule } from '@angular/router';
     TextInputComponent,
     CdkStepperModule,
     StepperComponent,
-    BasketSummaryComponent
+    BasketSummaryComponent,
+    FormsModule
   ]
 })
 export class SharedModule { }
